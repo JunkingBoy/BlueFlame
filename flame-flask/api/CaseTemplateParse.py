cf90caa7b210e6d4a9c41bb126f97a5171eb68e5
xcx <15815176957@163.com>
2024-06-21 22:21:59 +0800

    下载模版文件 接受上传文件 (未解析)

--- flame-flask/api/CaseTemplateParse.py
+++ flame-flask/api/CaseTemplateParse.py
@@ -1,14 +1,14 @@
 import os
 from flask_jwt_extended import jwt_required
 from utils.CommonResponse import R
-from flask import Blueprint, send_file
+from flask import Blueprint, request, send_file
 
 
 case = Blueprint('case', __name__)
 
 
 @case.route('/download/case_template', methods=["GET"])
-@jwt_required()
+# @jwt_required()
 def download_case_template_file():
     cwd = os.getcwd()
     module_cwd = os.path.dirname(os.path.realpath(__file__))
@@ -21,3 +21,16 @@ def download_case_template_file():
         return R.create(1234, "File not found") 
     finally:
         os.chdir(cwd)
+
+@case.route('/upload', methods=['POST'])
+@jwt_required()
+def upload_file():
+    if 'file' not in request.files:
+        return R.err('No file upload')
+    file = request.files['file']
+    if file.filename == '' or file.filename is None:
+        return R.err('No selected file')
+    if file:
+        file.save(os.path.join('.', file.filename))
+        # TODO<2024-06-21, @xcx> 添加解析模版的代码
+        return R.ok('File uploaded successfully')
--- flame-flask/utils/StringUtil.py
+++ flame-flask/utils/StringUtil.py
@@ -1,3 +1,4 @@
+import os
 import random
 import string
 
@@ -6,4 +7,19 @@ def generate_string(length: int) -> str:
     """
     Generate a random string of a specified length
     """
-    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
+    return "".join(
+        random.choices(string.ascii_letters + string.digits, k=length))
+
+
+def secure_filename(filename: str) -> str:
+    # 获取文件的基本名，删除路径信息
+    base_name = os.path.basename(filename)
+    # 删除不安全的字符
+    safe_chars = string.ascii_letters + string.digits + "_-."
+    safe_name = ''.join(c for c in base_name if c in safe_chars)
+    # 如果文件名为空或者只包含不安全的字符，生成一个随机的文件名
+    if not safe_name:
+        safe_name = ''.join(
+            random.choice(string.ascii_letters + string.digits)
+            for _ in range(10))
+    return safe_name
