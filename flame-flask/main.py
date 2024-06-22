9bb281d1f1eddc71336ed0df9cfd7aa496bcb2d4
xcx <15815176957@163.com>
2024-06-23 01:14:34 +0800

    hot fix

--- flame-flask/main.py
+++ flame-flask/main.py
@@ -11,5 +11,9 @@ JWTManager(app)
 db.init_app(app)
 register_routes(app)
 
+@app.before_request
+def create_tables():
+    db.create_all()
+
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
