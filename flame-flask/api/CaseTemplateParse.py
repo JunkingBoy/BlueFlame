9c9742b177d29314c92d6420d5ef60be1fda9b8d
junkingboy <junkingboy@163.com>
2024-06-23 20:13:58 +0800

    拓展获取测试用例模板api

--- flame-flask/api/CaseTemplateParse.py
+++ flame-flask/api/CaseTemplateParse.py
@@ -1,7 +1,7 @@
 import os
 from flask_jwt_extended import jwt_required
 from utils.CommonResponse import R
-from flask import Blueprint, send_file
+from flask import Blueprint, send_file, request
 
 
 case = Blueprint('case', __name__)
@@ -10,14 +10,29 @@ case = Blueprint('case', __name__)
 @case.route('/download/case_template', methods=["GET"])
 @jwt_required()
 def download_case_template_file():
+    '''
+    接收前端传过来的一个type字段
+    选择下载的excel类型
+    '''
     cwd = os.getcwd()
     module_cwd = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(module_cwd)
-    template_filepath = './static/case_template.xlsx'
-    try:
-        return send_file(template_filepath, as_attachment=True)
-    except FileNotFoundError as err:
-        print(f"File not found: {err}")
-        return R.create(1234, "File not found") 
-    finally:
-        os.chdir(cwd)
+    os.chdir(module_cwd) # flame-flask/api
+
+    type: str | None = request.args.get('type')
+
+    template_filepath: str = ''
+
+    match type:
+        case '1':
+            template_filepath = './static/case_template.xlsx'
+
+    if template_filepath != '':
+        try:
+            return send_file(template_filepath, as_attachment=True)
+        except FileNotFoundError as err:
+            print(f"File not found: {err}")
+            return R.create(1234, "File not found") 
+        finally:
+            os.chdir(cwd)
+    else:
+        return R.create(code=404, msg='type参数错误', data={})
