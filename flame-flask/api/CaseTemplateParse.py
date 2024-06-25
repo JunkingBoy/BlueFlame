2c56a61906caddffb11d67ca2194ed4c5166154a
xcx <15815176957@163.com>
2024-06-25 22:20:52 +0800

    case template 解析,添加类型注解,拆分函数

--- flame-flask/api/CaseTemplateParse.py
+++ flame-flask/api/CaseTemplateParse.py
@@ -2,7 +2,9 @@ import os
 from service.CaseTemplate import CaseTemplate
 from flask_jwt_extended import jwt_required
 from utils.CommonResponse import R
-from flask import Blueprint, send_file, request, Response, current_app
+from flask import Blueprint,Response,  request, send_file, current_app
+from werkzeug.datastructures import FileStorage
+
 
 case = Blueprint('case', __name__)
 
@@ -45,30 +47,19 @@ def is_valid_file(file):
 
 @case.route('/upload', methods=['POST'])
 @jwt_required()
-def upload_file() -> Response:
-    '''
-    根据type参数选择解析的模版
-    '''
-    type: str | None = request.args.get('type')
+def upload_file():
+    if 'file' not in request.files:
+        return R.err('No file upload')
+    case_type = request.args['type']
+    file: FileStorage = request.files['file']
+    if file.filename == '' or file.filename is None:
+        return R.err('No selected file')
+    if not is_valid_file(file.filename):
+        return R.err('Invalid file type')
 
-    if type is not None:
-        if 'file' not in request.files:
-            return R.err('No file upload')
-        file = request.files['file']
-        if file.filename == '' or file.filename is None:
-            return R.err('No selected file')
-        if not is_valid_file(file.filename):
-            return R.err('Invalid file type')
-        
-    match type:
-        case '1':
-            data = CaseTemplate(str(file), type).get_data()
-        case '2':
-            pass
-        case _:
-            return R.err('no such type')
-        
-    return R.create(code=200, msg='success', data={})
+    # print(f'file: {type(file)}')
+    data = CaseTemplate(file, case_type).get_data()
+    # print(f'data: {data}')
 
         # data = CaseTemplate(str(file)).get_data()
         # # print(f'data: {data}')
--- flame-flask/service/CaseTemplate.py
+++ flame-flask/service/CaseTemplate.py
@@ -1,70 +1,75 @@
-import openpyxl
-from openpyxl.worksheet.worksheet import Worksheet
+import io
 from typing import List, Dict, Any
+from openpyxl import load_workbook
+from openpyxl.worksheet.worksheet import Worksheet
+from werkzeug.datastructures import FileStorage
 from flask import current_app
 from utils.FindHeader import find_header
 
 class CaseTemplate:
 
-    def __init__(self, file_path: str, file_type: str):
-        self.file_path = file_path
-        self.data = None
-        self.case_type = file_type
-        self.parse()
-
-    def parse(self):
-        # 读取Excel 文件, 并解析数据成 list[dict[str, any]]
+    def __init__(self, file: FileStorage, case_type: str = ""):
+        self.file = file
+        self.case_type = case_type
         self.data = self.parse_case_template_excel()
 
     def get_data(self):
         return self.data
 
-    def parse_case_template_excel(
-            self,
-            sheet_name: str = None) -> List[Dict[str, Any]] | R:  # type: ignore
+    def parse_case_template_excel(self,
+                                  sheet_name: str = ""
+                                  ) -> List[Dict[str, Any]]:
         try:
-            # 加载Excel工作簿
-            workbook = openpyxl.load_workbook(self.file_path)
-
-            # 选择工作表
-            if sheet_name:
-                sheet: Worksheet = workbook[sheet_name]
-            else:
-                sheet: Worksheet = workbook.active  # type: ignore
-
-            # 获取表头
-            headers = [
-                cell.value
-                for cell in next(sheet.iter_rows(min_row=1, max_row=1))
-            ]
-
-            '''
-            根据传入的file_type选择需要对应的表头
-            '''
-            header_dict: Dict[str, Any] = find_header(self.case_type)
-
-            if header_dict is not {}:
-                data = []
-                # 从第二行开始读取数据
-                for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row):
-                    row_data = {}
-                    for cell, header in zip(row, headers):
-                        cell_value = cell.value
-                        for merged_cell_range in sheet.merged_cells:  # type: ignore
-                            if cell.coordinate in merged_cell_range:
-                                cell_value = sheet.cell(
-                                    row=merged_cell_range.min_row,
-                                    column=merged_cell_range.min_col).value
-                                break
-                        # 将中文表头转换为英文表头
-                        english_header = header_dict.get(header, header)  # type: ignore
-                        row_data[english_header] = cell_value
-                    data.append(row_data)
-                    return data
-            else:
-                current_app.logger.error(f"An error occurred while parsing the case template")
-                return []
-        except Exception as e:
-            current_app.logger.error(f"An error occurred while parsing the case template: {str(e)}")
+            # 将 FileStorage 对象的内容读取到内存中的 BytesIO 对象
+            file_stream = io.BytesIO(self.file.read())
             
-        return []
+            # 使用 BytesIO 对象加载工作簿
+            workbook = load_workbook(file_stream, data_only=True)
+            sheet = workbook[sheet_name] if sheet_name else workbook.active
+            headers = [cell.value for cell in next(sheet.iter_rows(min_row=1, max_row=1))]
+            header_dict = self.get_header_dict()
+            data = self.extract_data(sheet, headers, header_dict)
+            return data
+        except Exception as e:
+            print(f"解析模板时发生错误: {str(e)}")
+            return []
+
+    def get_header_dict(self) -> Dict[str, str]:
+        return {
+            '用例编号': 'case_id',
+            '用例名称': 'case_name',
+            '测试环境': 'test_environment',
+            '所属模块': 'module',
+            '前置条件': 'precondition',
+            '操作步骤': 'steps',
+            '预期结果': 'expected_result',
+            '测试结果': 'test_result',
+            '创建时间': 'create_time',
+            '修改时间': 'modify_time'
+        }
+
+    def extract_data(self, sheet: Worksheet, headers: List[str],
+                     header_dict: Dict[str, str]) -> List[Dict[str, Any]]:
+        data = []
+        merged_cells_value = self.get_merged_cells_value(sheet)
+        for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row):
+            row_data = {}
+            for cell, header in zip(row, headers):
+                cell_value = merged_cells_value.get(cell.coordinate,
+                                                    cell.value)
+                english_header = header_dict.get(header, header)
+                row_data[english_header] = cell_value
+            data.append(row_data)
+        return data
+
+    def get_merged_cells_value(self, sheet: Worksheet) -> Dict[str, Any]:
+        merged_cells_value = {}
+        for merged_range in sheet.merged_cell_ranges.ranges:
+            top_left_value = sheet.cell(merged_range.min_row,
+                                        merged_range.min_col).value
+            for row in range(merged_range.min_row, merged_range.max_row + 1):
+                for col in range(merged_range.min_col,
+                                 merged_range.max_col + 1):
+                    merged_cells_value[sheet.cell(
+                        row, col).coordinate] = top_left_value
+        return merged_cells_value
