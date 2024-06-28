07d59350565000077480ee03182a96d34a632a68
Xie ChaoXin <51036972+xiechaoxin@users.noreply.github.com>
2024-06-29 01:14:52 +0800

    hot fix,移除掉 api.funcCase (#12)

--- flame-flask/api/__init__.py
+++ flame-flask/api/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from .User import user
 from .CaseTemplateParse import case
-from .FuncCase import func_case
 from .Project import project
 
 
 def register_routes(app: Flask):
     app.register_blueprint(user, url_prefix="/user/")
     app.register_blueprint(case, url_prefix="/case/")
-    app.register_blueprint(func_case)
     app.register_blueprint(project, url_prefix="/project/")
