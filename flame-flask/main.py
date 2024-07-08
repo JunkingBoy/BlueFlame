214e53acbe7581955f44fb754fff17921174ce75
xcx <15815176957@163.com>
2024-07-08 15:52:38 +0800

    fix,移除无用的引用, create_db 在应用启动时候

--- flame-flask/main.py
+++ flame-flask/main.py
@@ -5,15 +5,15 @@ from Config import create_app
 from model import db
 from api import register_routes
 
-app: Flask = create_app()
-CORS(app)
-JWTManager(app)
-db.init_app(app)
-register_routes(app)
-
-@app.before_request
-def create_tables():
-    db.create_all()
 
 if __name__ == "__main__":
+    app: Flask = create_app()
+    CORS(app)
+    JWTManager(app)
+    db.init_app(app)
+    register_routes(app)
+
+    with app.app_context():
+        db.create_all()
+
     app.run(host='0.0.0.0', port=8000, debug=True)
--- flame-flask/model/__init__.py
+++ flame-flask/model/__init__.py
@@ -4,13 +4,5 @@ from sqlalchemy.engine import Engine
 
 db = SQLAlchemy(
     engine_options={"connect_args": {
-        "options": "-c timezone=utc"
+        "options": "-c timezone=Asia/Shanghai"
     }})
-
-
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    print("SETTING TIMEZONE")
-    cursor.execute("SET timezone TO 'UTC';")
-    cursor.close()
